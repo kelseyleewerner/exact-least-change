86ad8b81af7802acbb040a36eb55c0b30f538284
kelseyleewerner <kelsey.lee.werner@gmail.com>
2021-04-28 21:47:55 -0700

    memoized version of original recursive function

--- exact_least_change.c
+++ exact_least_change.c
@@ -5,6 +5,10 @@
 
 
 int basic_recursion(int);
+int memoized_recursion(int);
+
+
+int * t;
 
 
 int main()
@@ -14,7 +18,12 @@ int main()
   printf("Please provide a total number of pennies (>=1): ");
   scanf("%d", &n);
 
-  printf("Using Recusion: The number of coins to make exact least change for %d pennies is %d\n", n, basic_recursion(n));
+  t = malloc(sizeof(int) * (n + 1));
+
+  for (int i = 0; i <= n; ++i) { t[i] = -1; }
+
+  printf("Using Basic Recusion: The number of coins to make exact least change for %d pennies is %d\n", n, basic_recursion(n));
+  printf("Using Memoized Recusion: The number of coins to make exact least change for %d pennies is %d\n", n, memoized_recursion(n));
 }
 
 
@@ -35,6 +44,30 @@ int basic_recursion(int n)
 
   return 1 + min;
 }
+
+
+int memoized_recursion(int n)
+{
+  int a, b, c, min;
+
+  if (n == 0) { return 0; }
+  if (n < 0) { return INT_MAX; }
+
+  if (t[n] != -1) { return t[n]; }
+
+  a = memoized_recursion(n - 1);
+  b = memoized_recursion(n - 3);
+  c = memoized_recursion(n - 4);
+
+  min = a;
+  if (b < min) { min = b; }
+  if (c < min) { min = c; }
+
+  t[n] = 1 + min;
+
+  return t[n];
+}
+
 
 
 
